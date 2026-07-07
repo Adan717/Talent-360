7caf22647f0921b09bc4d1e0372e845924ff083e
pcmaster-prog <pcmaster-prog@users.noreply.github.com>
2026-07-06 23:52:31 -0600

    Corregir desestructuraciÃ³n de handleEarlyDepartureClick en RelojVisual

--- Frontend/src/components/reloj/RelojVisual.tsx
+++ Frontend/src/components/reloj/RelojVisual.tsx
@@ -335,6 +335,7 @@ export default function RelojVisual({
     setShowEarlyDepartureModal,
     earlyDepartureReason,
     setEarlyDepartureReason,
+    handleEarlyDepartureClick,
     submitEarlyDeparture,
     hasMealReservation,
     authorizeClockOutWithPendingTasks
